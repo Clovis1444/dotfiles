0e32d7fa1f92dc0a657425fea22f3ab59523a837
Clovis1444 <eu11111444@gmail.com>
2024-11-27 16:09:18 +0300

    feat(nvim): add glsl support

--- dot_config/nvim/lua/plugins/none-ls.lua
+++ dot_config/nvim/lua/plugins/none-ls.lua
@@ -2,20 +2,23 @@
 -- Note: all sources should be install via Mason
 
 return {
-    "nvimtools/none-ls.nvim",
-    config = function()
-        local null_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
 
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.clang_format,
-                null_ls.builtins.formatting.prettier,
-                -- null_ls.builtins.diagnostics.eslint,
-            },
-        })
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				-- Note: Also glsl shaders support
+				null_ls.builtins.formatting.clang_format.with({
+					filetypes = { "c", "cpp", "glsl", "vert", "frag", "tesc", "tese", "geom", "comp" },
+				}),
+				null_ls.builtins.formatting.prettier,
+				-- null_ls.builtins.diagnostics.eslint,
+			},
+		})
 
-        -- Global maps
-        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-    end,
+		-- Global maps
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
