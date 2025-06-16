2237ff7805b39c588db9389adba021859bffc2ef
Clovis1444 <eu11111444@gmail.com>
2025-06-16 17:05:47 +0300

    feat(nvim): add relative file path to lualine; minor changes in lualine config

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -1,7 +1,19 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("lualine").setup()
-	end,
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("lualine").setup({
+            options = {
+                theme = "auto",
+                -- Use this to enable lualine in buffers when switch focus to Neo-tree
+                ignore_focus = { "neo-tree" },
+            },
+            sections = {
+                -- Show filename and filepath relative to pwd
+                lualine_c = { "filename", [[vim.fn.expand("%:.")]] },
+            },
+            -- Add extension for Neo-Tree
+            extensions = { "neo-tree" },
+        })
+    end,
 }
