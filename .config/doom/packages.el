3c6c00b3a8a49a5c2a3302c0c48b709f38278828
clovis <eu11111444@gmail.com>
2026-02-13 20:22:09 +0300

    emacs: add kdl support

--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -15,6 +15,7 @@
 
 (package! catppuccin-theme)
 (package! drag-stuff)
+(package! kdl-mode)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
